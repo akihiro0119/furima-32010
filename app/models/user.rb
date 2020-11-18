479274dce827fe510b725b81671e8c51e25c835f
akihiro0119 <aono1x19@gmail.com>
2020-11-18 21:24:16 +0900

    正規表現の簡略化

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,15 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   with_options presence: true do
-  
-
-  validates :nickname
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: "is invalid. Input full-width characters."
-  validates :first_name, format: {with: /\A[ぁ-んァ-ン一-龥]/, message:"is invalid. Input full-width characters."}
-  validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
-  validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
-  validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
-  validates :birth_day 
+    validates :nickname
+    PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+    validates_format_of :password, with: PASSWORD_REGEX, message: "is invalid. Input full-width characters."
+    validates :first_name, :last_name, format: {with: /\A[ぁ-んァ-ン一-龥]/, message:"is invalid. Input full-width characters."}
+    validates :first_name_kana, :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
+    validates :birth_day 
   end
 end
