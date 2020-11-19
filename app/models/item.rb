6b42c80f841d8aaebded7c24372df12ff5bd9f45
akihiro0119 <aono1x19@gmail.com>
2020-11-19 13:10:41 +0900

    商品の登録機能実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,20 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :freight
+  belongs_to :area
+  belongs_to :shipdate
+  has_one_attached :image
+
+  validates :image, presence: true
+  validates :title, presence: true
+  validates :explanation, presence: true
+  validates :category_id, numericality: { other_than: 1 }
+  validates :status_id, numericality: { other_than: 1 }
+  validates :freight_id, numericality: { other_than: 1 }
+  validates :area_id, numericality: { other_than: 1 }
+  validates :shipdate_id, numericality: { other_than: 1 }
+
+
 end
